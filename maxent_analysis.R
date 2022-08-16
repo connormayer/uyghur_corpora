76aa1e5cc1362bd0dd213849b4e247a4a82718da
Connor Mayer <cjmayer@uci.edu>
2022-08-16 11:34:20 -0700

    Little changes

--- maxent_analysis.R
+++ maxent_analysis.R
@@ -3,8 +3,7 @@ require(archive)
 
 # Load in our data. If you want to run this script yourself, you'll need
 # to set the working directory to wherever you've checked out the corpora
-# setwd("E:/git_repos/uyghur_corpora")
-setwd("//wsl.localhost/Ubuntu/home/connor/uyghur_corpora")
+setwd("E:/git_repos/uyghur_corpora")
 
 input_file <- archive_read('corpora/raising_candidates.zip')
 raising_candidates <- read_csv(input_file, col_types = cols())
@@ -17,13 +16,13 @@ raising_candidates <- read_csv(input_file, col_types = cols())
 #     # The bobyqa optimizer has more luck converging than the default Nelder_Mead
 #     #control=glmerControl(optimizer = 'bobyqa')
 #   )
+raising_candidates <- raising_candidates %>%
+  mutate(last_one = substr(last_two, 2, 2))
 
 simple_model <- glm(
-  back_count ~ last_two * log_norm_count + last_two * raised_form_prop + last_two_distance + has_name + has_ane + has_che,
+  back_count ~ last_one * log_norm_count + last_one * raised_form_prop + last_two_distance + has_name + has_ane + has_che,
   data=raising_candidates,
-  family="binomial"#,
-  # The bobyqa optimizer has more luck converging than the default Nelder_Mead
-  #control=glmerControl(optimizer = 'bobyqa')
+  family="binomial"
 )
 
 raising_candidates$predicted_simple <- predict(simple_model)
@@ -224,108 +223,29 @@ for (i in 1:nrow(root_agg)) {
   write.table(matrix(raise_b, nrow=1), file=output_file, row.names=FALSE, col.names = FALSE, append=TRUE, sep=',')
 }
 
-
 # Oracle output
-output_file <- 'maxent_data/fully_indexed_output.csv'
-n_general_constraints <- 2
-n_total_constraints <- n_general_constraints + nrow(root_agg)
-headers <- c('', '', '', 'VAgree', '*Unraised')
+oracle <- root_agg %>% 
+  select(n, percent_back) %>%
+  mutate(back_count = n * percent_back,
+         front_count = n - back_count,
+         log_prob = case_when(back_count == 0 ~ front_count * log(1 - percent_back),
+                              front_count == 0 ~ back_count * log(percent_back),
+                              TRUE ~ back_count * log(percent_back) + front_count * log(1 - percent_back)))
 
-for (i in 1:nrow(root_agg)) {
-  headers <- c(
-    headers, 
-    paste('HarmonizeBack-', raising_candidates[i,]$root, sep=''),
-    paste('HarmonizeFront-', raising_candidates[i,]$root, sep=''))
-}
-write.table(matrix(headers, nrow=1), file=output_file, row.names=FALSE, col.names = FALSE, sep=',')
-write.table(matrix(headers, nrow=1), file=output_file, row.names=FALSE, col.names = FALSE, sep=',', append=TRUE)
-#write.table(matrix(c('', '', '', rep(0, n_total_constraints)), nrow=1), file=output_file, row.names=FALSE, col.names = FALSE, sep=',', append=TRUE)
+oracle_ll <- sum(oracle$log_prob)
+# k is the number of roots * 3 because there are four possible outcomes for each
+# root (unraised-b, raised-b, unraised-f, raised-f) and therefore 3 probabilities
+# to set for each root.
+oracle_k <- nrow(root_agg) * 3
+oracle_bic <- log(sum(root_agg$n)) * oracle_k - 2 * oracle_ll
 
-for (i in 1:nrow(root_agg)) {
-  row <- root_agg[i,]
-  no_raise_f <- c(row$root, paste(row$root, '-F', sep=''))
-  no_raise_b <- c('', paste(row$root, '-B', sep=''))
-  raise_f <- c('', paste(row$root, '-RAISE-F', sep=''))
-  raise_b <- c('', paste(row$root, '-RAISE-B', sep=''))
-  
-  # Add frequencies
-  no_raise_f <- c(no_raise_f, 0)
-  no_raise_b <- c(no_raise_b, 0)
-  raise_f <- c(raise_f, (row$n * (1 - row$percent_back)))
-  raise_b <- c(raise_b, (row$n * row$percent_back))
-  
-  # Calculate VAgree violations
-  if (row$last_two == 'BB') {
-    no_raise_f <- c(no_raise_f, 1)
-    no_raise_b <- c(no_raise_b, '')
-    raise_f <- c(raise_f, 1)
-    raise_b <- c(raise_b, '')
-  }
-  if (row$last_two == 'FF') {
-    no_raise_f <- c(no_raise_f, '')
-    no_raise_b <- c(no_raise_b, 1)
-    raise_f <- c(raise_f, '')
-    raise_b <- c(raise_b, 1)
-  }
-  if (row$last_two == 'FB') {
-    no_raise_f <- c(no_raise_f, 1)
-    no_raise_b <- c(no_raise_b, '')
-    raise_f <- c(raise_f, '')
-    raise_b <- c(raise_b, 1)
-  }
-  if (row$last_two == 'BF') {
-    no_raise_f <- c(no_raise_f, '')
-    no_raise_b <- c(no_raise_b, 1)
-    raise_f <- c(raise_f, 1)
-    raise_b <- c(raise_b, '')
-  }
-  
-  # Do raising violations
-  no_raise_f <- c(no_raise_f, 1)
-  raise_f <- c(raise_f, '')
-  no_raise_b <- c(no_raise_b, 1)
-  raise_b <- c(raise_b, '')
-  
-  # Add empty spaces for previous constraints
-  n_prev_constraints <- 2* (i - 1)
-  prev_vals <- rep('', n_prev_constraints)
-  no_raise_f <- c(no_raise_f, prev_vals)
-  no_raise_b <- c(no_raise_b, prev_vals)
-  raise_f <- c(raise_f, prev_vals)
-  raise_b <- c(raise_b, prev_vals)
-  
-  # Calculate HarmonizeBack violation
-  no_raise_f <- c(no_raise_f, 1)
-  no_raise_b <- c(no_raise_b, '')
-  raise_f <- c(raise_f, 1)
-  raise_b <- c(raise_b, '')
-  
-  
-  # Calculate HarmonizeFront violation
-  no_raise_f <- c(no_raise_f, '')
-  no_raise_b <- c(no_raise_b, 1)
-  raise_f <- c(raise_f, 1)
-  raise_b <- c(raise_b, 1)
-  
-  # Add empty spaces for following constraints
-  n_following_constraints <- 2 * (n_total_constraints - i)
-  following_vals <- rep('', n_following_constraints)
-  no_raise_f <- c(no_raise_f, following_vals)
-  no_raise_b <- c(no_raise_b, following_vals)
-  raise_f <- c(raise_f, following_vals)
-  raise_b <- c(raise_b, following_vals)
-  
-  write.table(matrix(no_raise_f, nrow=1), file=output_file, row.names=FALSE, col.names = FALSE, append=TRUE, sep=',')
-  write.table(matrix(no_raise_b, nrow=1), file=output_file, row.names=FALSE, col.names = FALSE, append=TRUE, sep=',')
-  write.table(matrix(raise_f, nrow=1), file=output_file, row.names=FALSE, col.names = FALSE, append=TRUE, sep=',')
-  write.table(matrix(raise_b, nrow=1), file=output_file, row.names=FALSE, col.names = FALSE, append=TRUE, sep=',')
-}
-
+# More conservative estimates for lexical models
+lexical_bic <- log(sum(root_agg$n)) * 18 - 2 * -10280
+lexical_surface_bic <- log(sum(root_agg$n)) * 19 - 2 * -10280
 
 # Fit maxent models
 # These take a while to run
 surface_true_m <- optimize_weights('maxent_data/surface_true_output.csv', in_sep=',', upper_bound=50)
 opaque_m <- optimize_weights('maxent_data/opaque_output.csv', in_sep=',', upper_bound = 50)
 indexed_m <- optimize_weights('maxent_data/indexed_output.csv', in_sep=',', upper_bound=50)
-oracle_m <- optimize_weights('maxent_data/fully_indexed_output.csv', in_sep=',', upper_bound=50)
 compare_models(surface_true_m, opaque_m, indexed_m, method='bic')
