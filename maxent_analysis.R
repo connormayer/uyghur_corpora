f775f5c1dcfac3736213cd8c6e4f5d2577f6bb4d
Connor Mayer <cjmayer@uci.edu>
2023-10-11 15:56:30 -0700

    Moved to using larger dataset

--- maxent_analysis.R
+++ maxent_analysis.R
@@ -11,48 +11,31 @@ library(lme4)
 
 # Load in our data. If you want to run this script yourself, you'll need
 # to set the working directory to wherever you've checked out the corpora
-setwd("E:/git_repos/uyghur_corpora")
-# setwd("C:/Users/conno/git_repos/uyghur_corpora")
+# setwd("E:/git_repos/uyghur_corpora")
+setwd("C:/Users/conno/git_repos/uyghur_corpora")
 
 ############################
 # LOAD AND PREPROCESS DATA #
 ############################
 
 # Load data we want to analyze
-input_file <- archive_read('corpora/raising_candidates.zip')
-raising_candidates <- read_csv(input_file, col_types = cols())
+input_file <- archive_read('corpora/full_data_maxent.zip')
+maxent_data <- read_csv(input_file, col_types = cols())
 
 # Create variable corresponding to final vowel
-raising_candidates <- raising_candidates %>%
+maxent_data <- maxent_data %>%
   mutate(last_one = substr(last_two, 2, 2))
 
-# Train simple logistic regression model to calculate P(hc|x)
-lexical_model <- glm(
-  back_count ~ last_one * log_norm_count * raised_form_prop + has_name + has_ane + has_che,
-  data=raising_candidates,
-  family="binomial"
-)
-# Get number of fitted parameters in lexical model for use in BIC later
-num_coefs <- length(coef(lexical_model))
-
-# lexical_model_me <- glmer(
-#   back_count ~ last_one * log_norm_count * raised_form_prop + has_name + has_ane + has_che + (1|root),
-#   data=raising_candidates,
-#   family="binomial"
-# )
-
-raising_candidates$p_hc_b <- predict(lexical_model, type='response')
-# raising_candidates$p_hc_b_me <- predict(simple_model_me, type='response')
-
 # Aggregate data for simpler analysis
-root_agg <- raising_candidates %>%
-  filter(raised) %>%
+root_agg <- maxent_data %>%
   group_by(
-    root, log_norm_count, raised_form_prop, last_two, last_two_distance, 
-    has_name, has_ane, has_che, p_hc_b
+    root, log_norm_count, raised_form_prop, last_two, last_two_distance, last_one, 
+    has_name, has_ane, has_che, raising_candidate, raised
   ) %>%
   summarize(n = sum(back_count + front_count),
-            percent_back = sum(back_count) / n)
+            percent_back = sum(back_count) / n) %>%
+  mutate(raised = ifelse(raised, 'raised_n', 'unraised_n')) %>%
+  pivot_wider(names_from = raised, values_from = c(n, percent_back), values_fill = 0) 
 
 ###########################
 # BUILD AND SAVE TABLEAUX #
@@ -60,7 +43,7 @@ root_agg <- raising_candidates %>%
 
 # Function that builds relevant tableaux given a list of constraints
 create_tableaux <- function(data, constraints, output_file) {
-  headers <- c('Input', 'Output', 'Frequency', constraints)
+  headers <- c('Input', 'Output', 'Frequency', constraints, '*Unraised', 'ID')
   write.table(
     matrix(headers, nrow=1), 
     file=output_file, 
@@ -70,40 +53,81 @@ create_tableaux <- function(data, constraints, output_file) {
   )
   for (i in 1:nrow(data)) {
     row <- data[i,]
-    raise_f <- c(row$root, paste(row$root, '-RAISE-F', sep=''))
-    raise_b <- c(row$root, paste(row$root, '-RAISE-B', sep=''))
+    # Check whether we expect root to raise. Two conditions: 
+    # - root occurs in environment that conditions raising
+    # - root is a root that raises. We can't identify this directly, but we
+    #   estimate it based on proportion of raised forms in corpus. Non-raisers
+    #   should never show any raising.
+    should_raise <- row$raising_candidate & row$raised_form_prop > 0
+    
+    root_name <- str_c(
+      row$root, 
+      ifelse(should_raise, 'RAISER', 'NON_RAISER'),
+      sep='-'
+    )
+    raise_f <- c(root_name, paste(root_name, '-RAISED-F', sep=''))
+    raise_b <- c(root_name, paste(root_name, '-RAISED-B', sep=''))
+    unraise_f <- c(root_name, paste(root_name, '-UNRAISED-F', sep=''))
+    unraise_b <- c(root_name, paste(root_name, '-UNRAISED-B', sep=''))
     
     # Add frequencies
-    raise_f <- c(raise_f, (row$n * (1 - row$percent_back)))
-    raise_b <- c(raise_b, (row$n * row$percent_back))
+    raise_f <- c(raise_f, round(row$n_raised_n * (1 - row$percent_back_raised_n)))
+    raise_b <- c(raise_b, round(row$n_raised_n * row$percent_back_raised_n))
+    unraise_f <- c(unraise_f, round(row$n_unraised_n * (1 - row$percent_back_unraised_n)))
+    unraise_b <- c(unraise_b, round(row$n_unraised_n * row$percent_back_unraised_n)) 
     
     if ('VAgreeSurface' %in% constraints) {
       if (row$last_two == 'BB') {
         raise_f <- c(raise_f, 1)
         raise_b <- c(raise_b, '')
+        unraise_f <- c(unraise_f, 1)
+        unraise_b <- c(unraise_b, '')
       }
       if (row$last_two == 'FF') {
         raise_f <- c(raise_f, '')
         raise_b <- c(raise_b, 1)
+        unraise_f <- c(unraise_f, '')
+        unraise_b <- c(unraise_b, 1)
       }
       if (row$last_two == 'FB') {
         raise_f <- c(raise_f, '')
         raise_b <- c(raise_b, 1)
+        unraise_f <- c(unraise_f, 1)
+        unraise_b <- c(unraise_b, '')
       }
       if (row$last_two == 'BF') {
         raise_f <- c(raise_f, 1)
         raise_b <- c(raise_b, '')
+        unraise_f <- c(unraise_f, '')
+        unraise_b <- c(unraise_b, 1)
       }
     }
-    
     if ('VAgreeUnderlying' %in% constraints) {
       raise_f <- c(raise_f, ifelse(row$last_two %in% c('FB', 'BB'), 1, ''))
       raise_b <- c(raise_b, ifelse(row$last_two %in% c('BF', 'FF'), 1, ''))
+      unraise_f <- c(unraise_f, ifelse(row$last_two %in% c('FB', 'BB'), 1, ''))
+      unraise_b <- c(unraise_b, ifelse(row$last_two %in% c('BF', 'FF'), 1, ''))
     }
-    
     if ('HarmonicUniformity' %in% constraints) {
       raise_f <- c(raise_f, row$p_hc_b)
       raise_b <- c(raise_b, 1 - row$p_hc_b)
+      unraise_f <- c(unraise_f, row$p_hc_b)
+      unraise_b <- c(unraise_b, 1 - row$p_hc_b)
+    }
+      
+    # *Unraised and Max violations
+    if (should_raise) {
+      # Occurs in a context where raising should occur and root is a raiser
+      raise_f <- c(raise_f, '', 1)
+      raise_b <- c(raise_b, '', 1)
+      unraise_f <- c(unraise_f, 1, '')
+      unraise_b <- c(unraise_b, 1, '')
+    } else {
+      # Occurs in a context where raising shouldn't apply or root is a non-raiser
+      raise_f <- c(raise_f, '', 1)
+      raise_b <- c(raise_b, '', 1)
+      unraise_f <- c(unraise_f, '', '')
+      unraise_b <- c(unraise_b, '', '')
     }
     
     write.table(
@@ -116,6 +140,22 @@ create_tableaux <- function(data, constraints, output_file) {
     )
     write.table(
       matrix(raise_b, nrow=1), 
+      file=output_file, 
+      row.names=FALSE, 
+      col.names = FALSE, 
+      append=TRUE, 
+      sep=','
+    )
+    write.table(
+      matrix(unraise_f, nrow=1), 
+      file=output_file, 
+      row.names=FALSE, 
+      col.names = FALSE, 
+      append=TRUE, 
+      sep=','
+    )
+    write.table(
+      matrix(unraise_b, nrow=1), 
       file=output_file, 
       row.names=FALSE, 
       col.names = FALSE, 
@@ -137,13 +177,6 @@ create_tableaux(
   root_agg, 
   c('VAgreeUnderlying'), 
   'maxent_data/opaque_output.csv'
-)
-
-# Create tableaux for lexical model
-create_tableaux(
-  root_agg, 
-  c('HarmonicUniformity'), 
-  'maxent_data/lexical_output.csv'
 )
 
 # Create tableaux for input-surface model
@@ -172,36 +205,115 @@ create_tableaux(
 ##########################
 
 sigmas_to_try <- c(
-  5, 4, 3, 2, 1, 0.5, 0.1
+  #5, 4, 3, 2, 1, 0.5, 0.1
+  2
 )
 mus_to_try <- rep(0, length(sigmas_to_try))
+k <- 5
 
 # Fit surface-true model
 surface_true_output <- read_csv('maxent_data/surface_true_output.csv')
 surface_true_cv_m <- cross_validate(
   surface_true_output,
-  k = 10,
+  k = k,
   mu_values = mus_to_try,
   sigma_values = sigmas_to_try
 )
+write_csv(surface_true_cv_m, 'maxent_data/model_results/surface_true.csv')
 
 # Fit input-oriented model
 opaque_output <- read_csv('maxent_data/opaque_output.csv')
 opaque_cv_m <- cross_validate(
   opaque_output,
-  k = 10,
+  k = k,
   mu_values = mus_to_try,
   sigma_values = sigmas_to_try
 )
+write_csv(opaque_cv_m, 'maxent_data/model_results/input.csv')
 
 # Fit input-surface-model
 opaque_surface_output <- read_csv('maxent_data/opaque_surface_output.csv')
 opaque_surface_cv_m <- cross_validate(
   opaque_surface_output,
-  k = 10,
+  k = k,
   mu_values = mus_to_try,
   sigma_values = sigmas_to_try
 )
+write_csv(opaque_surface_cv_m, 'maxent_data/model_results/input_surface.csv')
+
+###################################
+# BESPOKE K-FOLD CROSS-VALIDATION #
+###################################
+
+# Set seed for reproducibility
+set.seed(123456789)
+
+do_cross_validation <- function(data, k, constraints, model_name, model_folder, mus, sigmas, create_files=TRUE) {
+  row_count <- nrow(data)
+  randomized_data <- data[sample(row_count),]
+  randomized_data$partition <- rep(seq_len(k), ceiling(row_count / k))[1:row_count]
+  
+  for (held_out in 1:k) {
+    training <- randomized_data %>%
+      filter(partition != held_out) %>%
+      group_by(
+        root, log_norm_count, raised_form_prop, last_two, last_two_distance, last_one, 
+        has_name, has_ane, has_che, raising_candidate, raised
+      ) %>%
+      summarize(n = sum(back_count + front_count),
+                percent_back = sum(back_count) / n) %>%
+      mutate(raised = ifelse(raised, 'raised_n', 'unraised_n')) %>%
+      pivot_wider(names_from = raised, values_from = c(n, percent_back), values_fill = 0) 
+    
+    test <- randomized_data %>%
+      filter(partition == held_out) %>%
+      group_by(
+        root, log_norm_count, raised_form_prop, last_two, last_two_distance, last_one, 
+        has_name, has_ane, has_che, raising_candidate, raised
+      ) %>%
+      summarize(n = sum(back_count + front_count),
+                percent_back = sum(back_count) / n) %>%
+      mutate(raised = ifelse(raised, 'raised_n', 'unraised_n')) %>%
+      pivot_wider(names_from = raised, values_from = c(n, percent_back), values_fill = 0) 
+    
+    # Train simple logistic regression model to calculate P(hc|x). We're using
+    # proportions weighted by count rather than individual tokens to speed things
+    # up, because we need to train this model quite a few times.
+    # TODO FIX THIS
+    lexical_model <- glm(
+      percent_back ~ last_one * log_norm_count * raised_form_prop + has_name + has_ane + has_che,
+      data=training,
+      family="binomial",
+      weights=training$n
+    )
+    # Apply model to predict data
+    training$p_hc_b <- predict(lexical_model, type='response')
+    test$p_hc_b <- predict(lexical_model, newdata=test, type='response')
+    
+    # Create training tableau
+    training_file <- str_c(
+      model_folder, '/', model_name, '_training_', held_out, '.csv'
+    )
+    if (create_files) {
+      create_tableaux(training, constraints, training_file)
+    }
+    
+    # Create test tableau
+    test_file <- str_c(
+      model_folder, '/', model_name, '_test_', held_out, '.csv'
+    )
+    if (create_files) {
+      create_tableaux(test, constraints, test_file)
+    }
+    for (i in 1:length(mus)) {
+      training_tableaux <- read_csv(training_file)
+      test_tableaux <- read_csv(test_file)
+      fitted_m <- optimize_weights(training_tableaux, mu_scalar=mus[i], sigma_scalar=sigmas[i])
+      predictions <- predict_probabilities(test_tableaux, fitted_m$weights)
+    }
+  }
+}
+
 
 # Fit lexical model
 lexical_output <- read_csv('maxent_data/lexical_output.csv')
@@ -231,6 +343,9 @@ lexical_surface_opaque_cv_m <- cross_validate(
   mu_values = mus_to_try,
   sigma_values = sigmas_to_try
 )
+
+
+
 
 surface_true_cv_m
 opaque_cv_m
